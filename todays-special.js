472df04900d1c8a404b4bff77d72593d2d738f7e
Lisa Smith <103579905+lsmith0387@users.noreply.github.com>
2022-05-19 09:10:15 -0400

    Add files via upload

---
+++ todays-special.js
@@ -0,0 +1,26 @@
+const menu = {
+    _meal: '',
+    _price: 0,
+  
+    set meal(mealToCheck){
+      if(typeof mealToCheck==='string'){
+        return this._meal = mealToCheck;
+      }
+    },
+    set price(priceToCheck){
+      if(typeof priceToCheck==='number'){
+        return this._price = priceToCheck;
+      }
+    },
+    get todaysSpecial(){
+      if(this._meal && this._price){
+        return `Today's Special is ${this._meal} for $${this._price}!`;
+      }else{
+        return 'Meal or price was not set correctly!';
+      }
+    }
+  };
+  
+  menu._meal='spaghetti';
+  menu._price=12;
+  console.log(menu.todaysSpecial);
